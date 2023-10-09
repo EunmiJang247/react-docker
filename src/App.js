221b790f44b14c4a4cafc8c9f8153c8f09636cae
softforyou <softforyou247@gmail.com>
2023-10-09 23:58:38 +0900

    please

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          슬피울어dㅇㄴㄹㄴㅇㄹ요
         </a>
       </header>
     </div>
